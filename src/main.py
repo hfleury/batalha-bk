61aa6dc5e1257806c1553d4bb2a39070b3a69242
Henrique Cardoso <henrique@oniweb.com.br>
2025-03-08 02:20:04 +0100

    add the formatter blank, flak8 and mypy

--- src/main.py
+++ src/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
+def read_root() -> dict:
+    """Root endpoint
+
+    Returns:
+        dict: Message
+    """
     return {"message": "Naval Battle API"}
