2d1fdf91afb0dfed4b52a59c0a28a2b91fa20432
Henrique Cardoso <henrique@oniweb.com.br>
2025-05-31 14:56:09 +0200

    feat: add the action shoot

--- src/game/router.py
+++ src/game/router.py
@@ -1,13 +1,14 @@
 import json
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from typing import Dict
+from typing import Dict, Any
 
 from src.game.connection_manager import connection_manager
-from src.game.redis import save_player_board, get_player_board
+from src.game.redis import GameRedisRepository
 from src.player.models import Player
 
 router = APIRouter()
+redis_repo = GameRedisRepository()
 
 
 @router.websocket("/ws/connect")
@@ -45,19 +46,45 @@ async def websocket_connection(websocket: WebSocket) -> None:
                     game_id = payload.get("game_id")
                     players = payload.get("players", {})
 
-                    result = await start_game(game_id, players)
+                    result = start_game(game_id, players)
                     await websocket.send_json(result)
                 elif action == "get_game_info":
                     game_id = payload.get("game_id")
                     requested_player_id = payload.get("player_id")
                     if game_id is not None and requested_player_id is not None:
-                        result = get_player_board(game_id, requested_player_id)
+                        result = redis_repo.get_player_board(game_id, requested_player_id)
                         await websocket.send_json(result)
                     else:
                         await websocket.send_json({
                             "status": "error",
                             "message": "Missing game_id or player_id"
                         })
+                elif action == "shoot":
+                    game_id = payload["game_id"]
+                    player_id = payload["player_id"]
+                    target = payload["target"]
+
+                    opponent_id =  redis_repo.get_opponent_id(game_id, player_id)
+                    opponent_board =  redis_repo.get_player_board(game_id, opponent_id)
+
+                    hit_result: dict[str, Any] = {"status": "miss", "target": target}
+
+                    for ship_id, positions in opponent_board.items():
+                        if target in positions:
+                            redis_repo.save_hit(game_id, opponent_id, ship_id, target)
+                            # Check if the ship is sunk
+                            hits = redis_repo.get_player_hits(game_id, opponent_id)
+
+                            is_sunk = set(hits.get(ship_id, [])) == set(positions)
+                            hit_result = {
+                                "status": "hit",
+                                "target": target,
+                                "ship_id": ship_id,
+                                "sunk": is_sunk
+                            }
+                            break
+
+                    await websocket.send_json(hit_result)
                 else:
                     await connection_manager.broadcast(
                         f"Player {player_id} says: {data}",
@@ -78,17 +105,17 @@ async def websocket_connection(websocket: WebSocket) -> None:
         )
 
 
-async def place_ships(game_id: str, player_id: int, ships: dict[str, list[str]]) -> Dict[str, str]:
+def place_ships(game_id: str, player_id: int, ships: dict[str, list[str]]) -> Dict[str, str]:
     """
     Receives and stores player's ship placements.
     """
 
-    save_player_board(game_id, player_id, ships)
+    redis_repo.save_player_board(game_id, player_id, ships)
     # TODO trigger async DB persistence(message/event)
 
     return {"status": "OK", "message": f"Ships placed for player {player_id}"}
 
-async def start_game(game_id: str, players: dict[int, dict[str, list[str]]]) -> Dict[str, str]:
+def start_game(game_id: str, players: dict[int, dict[str, list[str]]]) -> Dict[str, str]:
     """
     Initializes a new game state by saving each player's ship board.
 
@@ -99,7 +126,7 @@ async def start_game(game_id: str, players: dict[int, dict[str, list[str]]]) -> 
 
     for player_id_str, ships in players.items():
         player_id = int(player_id_str)  # In case keys are strings in JSON
-        save_player_board(game_id, player_id, ships)
+        redis_repo.save_player_board(game_id, player_id, ships)
         # TODO: Save game start metadata in Redis/db
 
     return {"status": "OK", "message": f"Game {game_id} started"}
