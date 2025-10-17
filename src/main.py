df1e747a3ca2bf20b57cc374e96a78c32a301ef5
Henrique Cardoso <henrique@oniweb.com.br>
2025-10-17 14:20:40 +0200

    Add a lifespan to run before the main function and initialize the setting with the loggging:

--- src/main.py
+++ src/main.py
@@ -1,15 +1,45 @@
 """Main application entry point for the Game server."""
+import logging
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 
 from fastapi import FastAPI
 
 from src.api.websocket_handler import router
 from src.infra.logger import setup_logging
+from src.core.config import settings
 
 setup_logging()
-app = FastAPI()
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def log_startup_info(app: FastAPI) -> AsyncGenerator[None, None]:
+    logger.info(
+        f"✅ Server starting in '{settings.app.environment}' mode"
+    )
+    logger.info(
+        f"📊 Database: {settings.db.host}:{settings.db.port} | DB={settings.db.database}"
+    )
+    logger.info(f"🧩 Redis: {settings.redis.host}:{settings.redis.port}")
+    logger.info(f"🖨️ Logging level: {settings.log.log_level}")
+    yield
+
+
+app = FastAPI(
+    title="Batalha Naval API",
+    version="0.1.0",
+    debug=settings.app.debug,
+    lifespan=log_startup_info
+)
 app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        reload=settings.app.is_development or settings.app.is_local,
+    )
