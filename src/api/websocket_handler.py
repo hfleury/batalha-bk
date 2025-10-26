fac83ba55d216ddea56e11dca6cd09b7dc8f7e70
Henrique Cardoso <henrique@oniweb.com.br>
2025-10-26 13:52:47 +0100

    Convert object to dict to send to send_json

--- src/api/websocket_handler.py
+++ src/api/websocket_handler.py
@@ -71,7 +71,7 @@ async def websocket_connection(websocket: WebSocket) -> None:
                 initial_response: StandardResponse = await game_service.handle_action(
                     action, payload, player
                 )
-                await websocket.send_json(initial_response)
+                await websocket.send_json(initial_response.to_dict())
 
             # Continue listening for subsequent messages
             while True:
@@ -82,7 +82,7 @@ async def websocket_connection(websocket: WebSocket) -> None:
                 handle_response: StandardResponse = await game_service.handle_action(
                     action, payload, player
                 )
-                await websocket.send_json(handle_response)
+                await websocket.send_json(handle_response.to_dict())
 
         except json.JSONDecodeError as e:
             logger.error(f"[{trace_id}] Invalid JSON ERROR: {e}")
